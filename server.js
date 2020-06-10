e859485eabc6e1c38d1aa7695dc0f3bcebfc4576
Eliana-Rodrigues <eliana.silva12@fatec.sp.gov.br>
2020-06-10 05:54:40 +0000

    Versão com o CRUD em memória

--- server.js
+++ server.js
@@ -5,34 +5,37 @@ const server = express();
 server.use(express.json()); 
 
 const bebidas = [
-    { bebida: 'Vinho', marca: 'San_German', valor: 18.00 , quantidade:10},
-    { bebida: 'Whisky', marca: 'Red_Label', valor: 90.00, quantidade:3},
-    { bebida: 'Refrigerante', marca: 'Coca-Cola', valor: 6.00, quantidade:15},
-    { bebida: 'Suco', marca: 'Kmais', valor:10.00, quantidade:9},
-    { bebida: 'Cerveja', marca: 'Skol', valor:4.00, quantidade:20},
+    { bebida:'Vodka', marca: 'Balalaika', valor: 17.04 , volume: 1000, quantidade:2},
+    { bebida: 'Whisky', marca: 'Red_Label', valor: 90.00, volume: 750,quantidade:3},
+    { bebida: 'Refrigerante', marca: 'Coca-Cola', valor: 7.50, volume: 2000,quantidade:12},
+    { bebida: 'Suco', marca: 'Kmais', valor:10.90, volume: 900,quantidade:9},
+    { bebida: 'Cerveja', marca: 'Skol', valor:4.00, volume: 550 ,quantidade:24},
+    { bebida: 'Cachaça', marca: 'Corote', valor:4.00, volume: 500, quantidade:15},
 ]
 
-server.get('/bebidas', function(request, response) {
+server.get('/bebida', function(request, response) {
     response.json(bebidas);
 })
 
-server.post('/bebidas', function(request, response) {
-     const {bebida, valor, marca, quantidade} = request.body;
+server.post('/bebida', function(request, response) {
+     const {bebida, valor, marca, volume, quantidade} = request.body;
 
-    bebidas.push({bebida, valor, marca, quantidade});
+    bebidas.push({bebida, valor, marca, volume, quantidade});
     response.status(204).send();
 })
 
 server.put('/bebida/:id', function(request, response) {
     const id = request.params.id;
-    const {bebida, valor, marca, quantidade} = request.body; 
+    const {bebida, valor, marca, volume, quantidade} = request.body; 
 
     for(let i = 0; i < bebidas.length; i++) {
         if(bebidas [i].bebida == id) {
             bebidas[i].bebida = bebida;
             bebidas[i].valor = valor;
-             bebidas[i].marca =marca;
+            bebidas[i].marca = marca;
+             bebidas[i].volume = volume;
             bebidas[i].quantidade = quantidade;
+
             break;  
         }
     }
